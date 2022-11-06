c1d3200bf2913002c7bf2da572a0e09d8fbd53b8
Mark <markvertenberg@gmail.com>
2022-11-06 19:04:05 +0200

    Circle Program added

---
+++ circle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    const double PI = 3.14159;
+    double radius;
+    double circumference;
+    double area;
+
+    printf("Enter a radius\n");
+    scanf("%lf", &radius);
+
+    circumference = 2 * PI * radius;
+    area = PI * radius * radius;
+
+    printf("Circumference: %lf\nArea: %lf", circumference, area);
+
+    return 0;
+}
